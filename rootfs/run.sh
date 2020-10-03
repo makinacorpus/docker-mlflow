874363a68e7e4cf29f447825d5cc69adfa895881
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2020-10-03 21:41:24 +0200

    expose docker host

--- rootfs/run.sh
+++ rootfs/run.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 cd "$(dirname $(readlink -f "$0"))"
-set -ex
+set -e
 
 # failed if undefined, see dockerfile
 export MLFLOW__HOST="${MLFLOW__HOST?}"
 export MLFLOW__PORT="${MLFLOW__PORT?}"
 export MLFLOW__BACKEND_STORE_URI="${MLFLOW__BACKEND_STORE_URI?}"
 export MLFLOW__ARTIFACTS_ROOT_URI="${MLFLOW__ARTIFACTS_ROOT_URI?}"
+export MLFLOW_SKIP_EXPOSE_HOST=${MLFLOW_SKIP_EXPOSE_HOST-}
 
 die() { echo $@ >&2; exit 1; }
 
@@ -19,7 +20,12 @@ fi
 if [ -e /data ];then
     chown mlflow /data
 fi
-
+if [[ -z ${MLFLOW_SKIP_EXPOSE_HOST} ]];then
+    ip -4 route list match 0/0 \
+        | awk '{print $3" host.docker.internal"}' >> /etc/hosts
+     export DOCKER_HOST_IP=$(ip -4 route list match 0/0 \
+        | awk '{print $3}')
+fi
 
 frep mlflow.frep:/etc/supervisor.d/mlflow --overwrite
 
